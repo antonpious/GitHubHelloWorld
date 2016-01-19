056ec3c7ba5af2b4eb669483f0fda70cfe71ed95
Anton Pious Alfred <antonpious@gmail.com>
2016-01-19 14:02:48 -0600

    Checking how to checkin checkout from Eclipse

--- gremlinserver-sample/src/main/java/com/anton/gremlinserver/sample/App.java
+++ gremlinserver-sample/src/main/java/com/anton/gremlinserver/sample/App.java
@@ -8,9 +8,16 @@ public class App
     public static void main( String[] args )
     {
     	Gson jsonSerializer = null;
+    	EntityModel model = null;
     	
     	jsonSerializer = new Gson();
     	
+    	model = new EntityModel();
+    	
         System.out.println(jsonSerializer.toJson("GitHub Checkin Hello World!"));
+        
+        System.out.println("Model :" + model.generateObject());
+        
+        
     }
 }
---
+++ gremlinserver-sample/src/main/java/com/anton/gremlinserver/sample/EntityModel.java
@@ -0,0 +1,16 @@
+package com.anton.gremlinserver.sample;
+
+public class EntityModel {
+	
+	public String generateObject()
+	{
+		String status = "";
+		
+		status = "Object Generated Successfully";
+		
+		return status;
+		
+		
+	}
+
+}
